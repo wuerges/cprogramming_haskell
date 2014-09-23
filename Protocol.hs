0133a88460645233e8b0af0a384fdcd9e282344d
Emilio Wuerges <wuerges@gmail.com>
2014-09-23 07:52:12 -0300

    worked code to allow empty response

--- Protocol.hs
+++ Protocol.hs
@@ -75,7 +75,7 @@ attendRequest mvar (OfferFile op fh) =
     do ps <- readMVar mvar
        let ps' = ps { m_dht_files = addItemDHT (mkItemDHT fh op) (ts $ m_dht_files ps) } 
        modifyMVar_ mvar (\ps -> return ps')
-       return Nothing
+       return LocalResponse
        
 attendRequest mvar (GetPeersRequest sender) = 
     do modifyMVar_ mvar (ps_add_peersIO $ Set.singleton sender)
